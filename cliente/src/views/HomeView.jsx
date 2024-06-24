4d10edf2b8eca01780bc9e65360c8b55248dfb08
Miguel Cordero <mikeycordero10@icloud.com>
2024-06-24 00:06:27 -0700

    Funcionalidad eliminarVenta lista

--- cliente/src/views/HomeView.jsx
+++ cliente/src/views/HomeView.jsx
@@ -5,6 +5,19 @@ import "./css/HomeView.css";
 
 function HomeView() {
   const [ventas, setVentas] = useState([]);
+  const [confirmacionVisible, setConfirmacionVisible] = useState(false);
+  const [ventaAEliminar, setVentaAEliminar] = useState(null);
+
+  const eliminarVenta = async (id) => {
+    try {
+      await axios.delete(`http://192.168.1.6:3000/api/ventas/${id}`);
+      setVentas(ventas.filter((venta) => venta.id !== id));
+      setConfirmacionVisible(false);
+      setVentaAEliminar(null);
+    } catch (error) {
+      console.error("Error al eliminar la venta:", error);
+    }
+  };
 
   useEffect(() => {
     const fetchVentas = async () => {
@@ -12,16 +25,54 @@ function HomeView() {
         const response = await axios.get("http://192.168.1.6:3000/api/ventas");
         setVentas(response.data);
       } catch (error) {
-        console.error("Error al establecer conexion con la API:", error);
+        console.error("Error al establecer conexión con la API:", error);
       }
     };
 
     fetchVentas();
   }, []);
 
+  const mostrarConfirmacion = (id) => {
+    const ventaSeleccionada = ventas.find((venta) => venta.id === id);
+
+    setVentaAEliminar(ventaSeleccionada);
+    setConfirmacionVisible(true);
+  };
+
+  const cancelarEliminacion = () => {
+    setConfirmacionVisible(false);
+    setVentaAEliminar(null);
+  };
+
   return (
     <>
       <main className="container mt-5">
+        <div
+          className={`alert alert-danger div-mensaje-confirmacion ${
+            confirmacionVisible ? "visible" : "oculto"
+          }`}
+        >
+          {ventaAEliminar && (
+            <>
+              <h4>
+                ¿Seguro que deseas eliminar la venta del cliente <br />
+                {ventaAEliminar.cliente}?
+              </h4>
+              <p></p>
+            </>
+          )}
+          <div className="div-confirmar-eliminacion">
+            <button
+              className="btn btn-danger"
+              onClick={() => eliminarVenta(ventaAEliminar.id)}
+            >
+              Eliminar
+            </button>
+            <button className="btn btn-primary" onClick={cancelarEliminacion}>
+              Cancelar
+            </button>
+          </div>
+        </div>
         <section className="card">
           <article className="card-header art-encabezado">
             <h4>Ventas</h4>
@@ -41,6 +92,7 @@ function HomeView() {
               <table className="table table-bordered table-striped">
                 <thead>
                   <tr>
+                    <th>Folio</th>
                     <th>Cliente</th>
                     <th>Servicio</th>
                     <th>Piezas</th>
@@ -54,6 +106,7 @@ function HomeView() {
                 <tbody>
                   {ventas.map((venta) => (
                     <tr key={venta.id}>
+                      <td>{venta.id}</td>
                       <td>{venta.cliente}</td>
                       <td>{venta.servicio}</td>
                       <td>{venta.pieza} pzs</td>
@@ -65,9 +118,12 @@ function HomeView() {
                         <Link to="#" className="btn btn-primary">
                           Editar
                         </Link>
-                        <Link to="#" className="btn btn-danger">
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => mostrarConfirmacion(venta.id)}
+                        >
                           Eliminar
-                        </Link>
+                        </button>
                       </td>
                     </tr>
                   ))}
